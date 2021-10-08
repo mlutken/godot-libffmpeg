c36c361f297dd9832863a9616593e8d3be5d32c3
Martin Lütken <martin@lutken.dk>
2021-10-08 19:35:38 +0200

    Made libffmpeg a "clean" ffmpeg" lib only

--- register_types.h
+++ register_types.h
@@ -1,7 +1,7 @@
-#ifndef MODULES_FFMPEG_REGISTER_TYPES_H
-#define MODULES_FFMPEG_REGISTER_TYPES_H
+#ifndef MODULES_LIBFFMPEG_REGISTER_TYPES_H
+#define MODULES_LIBFFMPEG_REGISTER_TYPES_H
 
-void register_ffmpeg_types();
-void unregister_ffmpeg_types();
+void register_libffmpeg_types();
+void unregister_libffmpeg_types();
 
-#endif // MODULES_FFMPEG_REGISTER_TYPES_H
+#endif // MODULES_LIBFFMPEG_REGISTER_TYPES_H
